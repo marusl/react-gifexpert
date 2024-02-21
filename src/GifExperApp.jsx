6c930135c7b507c657d702e923ac1acc45afad46
MariaE <maeusale@gmail.com>
2024-02-20 19:59:03 -0500

    First commit

---
+++ src/GifExperApp.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+const GifExperApp = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+
+        setCategories([newCategory, ...categories])
+    }
+
+    return (
+        <>
+            <h1>Gif Expert App</h1>
+
+            <AddCategory
+                onNewCategory={onAddCategory} />
+
+            {
+                categories.map(category =>
+                    <GifGrid
+                        key={category}
+                        category={category} />)
+            }
+        </>
+    )
+}
+
+export default GifExperApp;
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import GifExperApp from './GifExperApp';
+
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <GifExperApp/>
+)
